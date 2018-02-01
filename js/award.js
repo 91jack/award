9542a30440630a4048cf7206896c6ca69403545a
TaoSir1213 <2764132626@qq.com>
2018-02-01 12:46:26 +0800

    res
    res

--- js/award.js
+++ js/award.js
@@ -2,6 +2,8 @@ var myNumber;
 var result = [];//抽奖结果
 var code = [];//总人数
 var all = '';
+var localstroage = window.localStorage;  
+
 $('#allCount').change(function(){
 	all = $('#allCount').val();
 	$(this).attr('disabled','disabled');
@@ -26,63 +28,72 @@ function showRandomNum(num) {
     $(".prize_list ul").html(li);
 }
 
-$(function () {
-    $(".start").click(function(){
-      if($("#prize_btn").val() == 0){        
-        if($("#set_grade").val() == "选择奖项") {
-            alert("请选择奖项");
-            return;
-        }else if($("#prizeCount").val() == "") {
-            alert("请输入中奖人数");
-            return;
-        }else if($("#prizeCount").val() > 10) {
-            alert("单次抽奖人数不能超过10人");
-            return;
-        }else{
-        	if(code.length <= 0 || code.length <= 5){
-		    	alert("抽奖人数不能为0或小于0或小于5;\n抽奖结束")
-		    }else{
-		    	$("#prize_btn").val(1);
-	          	var num = $("#prizeCount").val();
-	          	$(this).find("img").attr("src","images/prize_stop.png");
-	
-		        myNumber = setInterval(function(){
-		            showRandomNum(num);
-		        }, 10);
-		        voice.src = "m1.mp3";
-		        voice.play();
-		          
-		        $('#allCount').val(code.length);
-		    }
-        }   
-    }else{
-		voice.src = "m02.wav";
-		voice.play();
-        $("#prize_btn").val(0);
-      	var arr = [];//当前中奖号码
-        $('.vetically li').each(function(){
-        	var n = parseInt($(this).text());
-        	code.splice($.inArray(n,code),1)
-        	arr.push(n)
-        });
-        var k = $(".prize_grade span").text();
-        console.log(k)
-        var now = {
-        	name: k,
-        	data: arr
-        }
-        result.push(now);
-        console.log("中奖号码："+arr);
-      	console.log("剩余抽奖人数为:"+code.length);
-      	
-        clearInterval(myNumber);
-        $(this).find("img").attr("src","images/prize_start.png");
-		console.log(result)
-      }    
-     
-    });
 
-    $("#set_grade").change(function(){
-      $(".prize_grade span").text($(this).val());
+$(".start").click(function(){
+  if($("#prize_btn").val() == 0){        
+    if($("#set_grade").val() == "选择奖项") {
+        alert("请选择奖项");
+        return;
+    }else if($("#prizeCount").val() == "") {
+        alert("请输入中奖人数");
+        return;
+    }else if($("#prizeCount").val() > 10) {
+        alert("单次抽奖人数不能超过10人");
+        return;
+    }else{
+    	if(code.length <= 0 || code.length <= 5){
+	    	alert("抽奖人数不能为0或小于0或小于5;\n抽奖结束")
+	    }else{
+	    	$("#prize_btn").val(1);
+          	var num = $("#prizeCount").val();
+          	$(this).find("img").attr("src","images/prize_stop.png");
+
+	        myNumber = setInterval(function(){
+	            showRandomNum(num);
+	        }, 10);
+	        voice.src = "m1.mp3";
+	        voice.play();
+	        localStorage.clear(); 
+	        $('#allCount').val(code.length);
+	    }
+    }   
+}else{
+	voice.src = "m02.wav";
+	voice.play();
+    $("#prize_btn").val(0);
+  	var arr = [];//当前中奖号码
+    $('.vetically li').each(function(){
+    	var n = parseInt($(this).text());
+    	code.splice($.inArray(n,code),1)
+    	arr.push(n)
     });
-}); 
+    var k = $(".prize_grade span").text();
+    console.log(k)
+    var now = {
+    	name: k,
+    	data: arr
+    }
+    result.unshift(now);
+    console.log("中奖号码："+arr);
+  	console.log("剩余抽奖人数为:"+code.length);
+  	
+    clearInterval(myNumber);
+    $(this).find("img").attr("src","images/prize_start.png");
+    
+	console.log(result)
+	localstroage.setItem('result',JSON.stringify(result));
+  }    
+ 
+});
+
+$("#set_grade").change(function(){
+  $(".prize_grade span").text($(this).val());
+});
+
+
+//页面刷新时调用
+//$(window).on('beforeunload', function(event) {
+//  localStorage.clear(); 
+//});
+
+  
